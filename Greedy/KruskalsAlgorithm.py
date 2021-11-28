b63317b9eaf683cf528780a4aaf0ea6499d742b7
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-11-28 21:37:46 +0530

    Add docstring in KruskalsAlgorithm

--- Greedy/KruskalsAlgorithm.py
+++ Greedy/KruskalsAlgorithm.py
@@ -1,8 +1,17 @@
 class Kruskals:
+    """
+    First we point every node pointing to itself by -1. Then we go through the
+    graph and take the minimum edge one and check if there is any cycle by
+    finding the parent by checking it is below zero or not then if we winf that
+    both has different parent then we make a union of them.
+    Union: We do union by findign the parent of both the node and comparing
+    which one has the less value and make it as parent then putting the
+    parent's index as other's value.
+    """
+
     def __init__(self, vertices) -> None:
         self.v = vertices
         self.graph = []
-        self.answer = []
         self.parent = [-1] * vertices
 
     def addEdge(self, src, dst, weight) -> None:
